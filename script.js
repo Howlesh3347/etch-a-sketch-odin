6607ac5a2a41a9d4777f4b3843436bfe6bb3a946
Talha <howlesh.3347@gmail.com>
2024-08-01 19:10:48 +0500

    Make Code Optimizations (Mentioned in Description)
    1. Make the sketch color randomized (rainbow effect)
    2. Opacity value limited to 1.0 instead of increasing indefinitely
    3. Place the individual pixel size calculation outside the loop to prevent unnecessary computations
    4. Fix Math.random expression not producing 255 rgb value
    5. Rename getPixel function to createPixel to better readability

--- script.js
+++ script.js
@@ -6,48 +6,53 @@ const resetButton = document.querySelector("#reset-btn");
 
 let canvasResolution = 50;
 
-getPixel(canvasResolution, false);
+createPixel(canvasResolution, false);
 
 resetButton.addEventListener("click", resetCanvas);
 
-function resetCanvas() {                                        // Output : Ask for New Resolution and Create new Canvas
-    newPixelCount = prompt("Specify the New Canvas Resolution", "");
-    return getPixel(newPixelCount, true);
-} 
-
-function colorRandomize() {                                     // Output : Give Random RGB Value
-    randomColor = Math.floor(Math.random() * 255);
-    return randomColor;
+function resetCanvas() {
+  // Output : Ask for New Resolution and Create new Canvas
+  newPixelCount = prompt("Specify the New Canvas Resolution", "");
+  createPixel(newPixelCount, true);
 }
 
-function getPixel(pixelCount, reset) {                         // Output : Square Container populated with square boxes
-    
-    canvasResolution = pixelCount;
-    let opacityValue = 0.1;
-
-    if (reset === true) {
-        sketchContainer.remove();
-        sketchContainer = document.createElement("div");
-        sketchContainer.setAttribute("class", "containerCss");
-        bodyContainer.appendChild(sketchContainer);
-    }
-
-    for (i=0; i<(pixelCount * pixelCount) ; i++) {
-        const pixel = document.createElement("div");
-        pixel.classList.toggle("pixel");
-        pixel.setAttribute("class", "pixelCss");
-
-        pixel.style.width = `${600/pixelCount}px`;
-        pixel.style.height = `${600/pixelCount}px`;
-
-        pixel.addEventListener("mouseenter", () => {
-            pixel.style.backgroundColor = `black`;
-            pixel.style.opacity = opacityValue;
-            opacityValue += 0.1;
-        });
-            
-        sketchContainer.appendChild(pixel);
+function colorRandomize() {
+  // Output : Give Random RGB Value
+  randomColor = Math.floor(Math.random() * 256);
+  return randomColor;
 }
-    resetButton.textContent = `The current Canvas Resolution is ${canvasResolution} x ${canvasResolution}.
+
+function createPixel(pixelCount, reset) {
+  // Output : Square Container populated with square boxes
+
+  canvasResolution = pixelCount;
+  let opacityValue = 0.1;
+  pixelWidth = 600 / pixelCount;
+  pixelHeight = 600 / pixelCount;
+
+  if (reset === true) {
+    sketchContainer.remove();
+    sketchContainer = document.createElement("div");
+    sketchContainer.setAttribute("class", "containerCss");
+    bodyContainer.appendChild(sketchContainer);
+  }
+
+  for (i = 0; i < pixelCount * pixelCount; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.toggle("pixel");
+    pixel.setAttribute("class", "pixelCss");
+
+    pixel.style.width = `${pixelWidth}px`;
+    pixel.style.height = `${pixelHeight}px`;
+
+    pixel.addEventListener("mouseenter", () => {
+      pixel.style.backgroundColor = `rgb(${colorRandomize()}, ${colorRandomize()}, ${colorRandomize()})`;
+      pixel.style.opacity = opacityValue;
+      opacityValue = Math.min(opacityValue + 0.1, 1.0);
+    });
+
+    sketchContainer.appendChild(pixel);
+  }
+  resetButton.textContent = `The current Canvas Resolution is ${canvasResolution} x ${canvasResolution}.
     You can change it by pressing this button`;
 }
